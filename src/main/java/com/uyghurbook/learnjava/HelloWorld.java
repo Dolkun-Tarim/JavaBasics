b842e2164c9a93c4a454cbcd09e4265553dff7ec
seleniummaster <services@seleniummaster.com>
2018-09-23 12:55:19 -0400

    Initial commit

---
+++ src/main/java/com/uyghurbook/learnjava/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.uyghurbook.learnjava;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("This is my first Java Program");
+        System.out.println("بۇ مەن يازغان بىرىنچى جاۋا پىروگىرام");
+        System.out.print("*****");
+        System.out.print("*****");
+    }
+
+
+}
+
+
