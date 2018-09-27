2338ede77955d43aa8e5b0d78bc9255a64b99f15
seleniummaster <services@seleniummaster.com>
2018-09-27 09:46:46 -0400

    Java print Uyghur

---
+++ src/main/java/com/uyghurbook/learnjava/UyghurStringDemo.java
@@ -0,0 +1,13 @@
+package com.uyghurbook.learnjava;
+
+public class UyghurStringDemo {
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("جاۋا پىروگىراممسىدا ئۇيغۇرچىنى چىقىرىش ");
+        //
+        System.out.println("\uFE8B"+"\uFE8E"+"\uFEE7"+"\uFE8E");
+        System.out.println("\uFE97"+"\uFBE9"+"\uFEDE");
+    }
+}
