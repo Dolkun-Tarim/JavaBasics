d61b13080b7e282f2f7b4abb9309dd5a6b051177
Selenium Master <services@seleniummaster.com>
2018-10-15 08:33:12 -0400

    Java char boolean data type

---
+++ src/main/java/com/uyghurbook/learnjava/BooleanDemo.java
@@ -0,0 +1,19 @@
+package com.uyghurbook.learnjava;
+
+public class BooleanDemo {
+
+    public static void main(String[] args)
+    {
+        boolean b1=true;
+        boolean b2=true;
+        boolean b3=false;
+        System.out.println(b1&&b2);
+        System.out.println(b1&&b3);
+
+        int x=50;
+        int y=55;
+        int z=30;
+        System.out.println((x>y)&&(x>z));
+
+    }
+}
---
+++ src/main/java/com/uyghurbook/learnjava/CharDemo.java
@@ -0,0 +1,15 @@
+package com.uyghurbook.learnjava;
+
+public class CharDemo {
+
+    public static void main(String[] args)
+    {
+        String s1="I am an Uyghur";
+        System.out.println(s1.indexOf("I"));
+        char firstLetter=s1.charAt(0);
+        //I ni besip chiqirish
+        System.out.println(firstLetter);
+        //r ni besip chiqirish
+        System.out.println(s1.charAt(s1.indexOf("r")));
+    }
+}
