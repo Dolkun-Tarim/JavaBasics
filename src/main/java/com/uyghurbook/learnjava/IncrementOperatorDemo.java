875330c79866236c44bad5b0d47e330cc5001d25
Selenium Master <services@seleniummaster.com>
2018-10-31 19:48:17 -0400

    Java Increment Operator

---
+++ src/main/java/com/uyghurbook/learnjava/IncrementOperatorDemo.java
@@ -0,0 +1,14 @@
+package com.uyghurbook.learnjava;
+
+public class IncrementOperatorDemo {
+    public static void main(String[] args) {
+        int i1=1;
+        int j1=1;
+        i1++; //i1=i1+1;
+        i1++;
+        j1--;
+        j1++;
+        System.out.println(String.format("i1=%d",i1));
+        System.out.println(String.format("j1=%d",j1));
+    }
+}
