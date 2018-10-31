e96935c367b18a29849447c4e854f6c3894e1528
Selenium Master <services@seleniummaster.com>
2018-10-31 19:34:17 -0400

    Java Arithmetic Operator

---
+++ src/main/java/com/uyghurbook/learnjava/MathDemo.java
@@ -0,0 +1,13 @@
+package com.uyghurbook.learnjava;
+
+public class MathDemo {
+    public static void main(String[] args) {
+        System.out.println("10 to the power of 3 "+Math.pow(10,2));
+        System.out.println("Square root of 10 "+Math.sqrt(9));
+        System.out.println("Return the max: "+Math.max(10,1000));
+        System.out.println("Return the min: "+Math.min(10,100));
+        System.out.println("Random number: "+Math.random());
+        System.out.println("Random number: "+Math.random());
+        System.out.println("Return the closest integer "+Math.round(3.4));
+    }
+}
---
+++ src/main/java/com/uyghurbook/learnjava/RelationalOperatorDemo.java
@@ -0,0 +1,20 @@
+package com.uyghurbook.learnjava;
+
+public class RelationalOperatorDemo {
+    public static void main(String[] args) {
+        int value1=10;
+        int value2=9;
+        //relational operator returns boolean type
+        System.out.println(value1==value2);
+        System.out.println(value1!=value2); //true
+        System.out.println(value1>value2);
+        System.out.println(value1<value2);
+        System.out.println("apple"=="apple");
+        System.out.println("Apple"=="apple");
+        System.out.println(value1!=value2);
+        double d1=10.50;
+        double d2=10.49;
+        System.out.println(d1>=d2);
+        System.out.println(d1<=d2);
+    }
+}
