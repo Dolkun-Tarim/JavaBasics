0702871e39e8043d54c5c74e5f5886ea6fa55d8a
Selenium Master <services@seleniummaster.com>
2018-10-31 20:00:06 -0400

    Java Boolean Operator

---
+++ src/main/java/com/uyghurbook/learnjava/BooleanDemo2.java
@@ -0,0 +1,14 @@
+package com.uyghurbook.learnjava;
+
+public class BooleanDemo2 {
+    public static void main(String[] args) {
+        int number1=5;
+        int number2=10;
+        //conditional operator AND (&&) OR (||)
+        boolean result=(number1==5)&&(number2==9);
+        System.out.println(result);
+        System.out.println((0<100)&&(200<100));
+        System.out.println((0<100)||(200<100));
+
+    }
+}
