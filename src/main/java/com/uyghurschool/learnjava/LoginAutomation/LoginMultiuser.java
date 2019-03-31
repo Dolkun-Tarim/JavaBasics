d6ce3014c75f00d4778cc6434bdb61d643059394
Selenium Master <services@seleniummaster.com>
2019-03-30 21:59:02 -0400

    Java Selenium WebDriver Login Automation with Multiple Users

---
+++ src/main/java/com/uyghurschool/learnjava/LoginAutomation/LoginMultiuser.java
@@ -0,0 +1,22 @@
+package com.uyghurschool.learnjava.LoginAutomation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginMultiuser {
+    public static void main(String[] args) {
+        List<User> users=new ArrayList<User>();
+        users.add(new User("admin","MaharaDemo"));
+        users.add(new User("student","MaharaDemo"));
+        for(User user:users) {
+            FunctionLibrary functionLibrary = new FunctionLibrary();
+            functionLibrary.openBrowser("https://demo.mahara.org");
+            functionLibrary.enterUsername(user.getUsername());
+            functionLibrary.enterPassword(user.getPassword());
+            functionLibrary.clickOnSubmitButton();
+            functionLibrary.loggedIn();
+            functionLibrary.logOut();
+            functionLibrary.closeTheBrowser();
+        }
+    }
+}
